9b96ef2f14fe8710a58835f53de2e9b82da0c979
Dalecia <petersondalecia@gmail.com>
2024-07-29 06:02:21 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "red", "Melancholic", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 5, "M", "black", "Melancholic", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 8, "F", "white", "Melancholic", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 10, "M", "red", "Melancholic", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 23, "F", "brown", "Melancholic", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 28, "M", "brown", "Melancholic", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 25, "F", "brown", "Melancholic", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 21, "M", "red", "Melancholic", True);
